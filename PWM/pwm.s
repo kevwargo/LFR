fdc626e0db6c50e70bda6ea1d7533cb0143d3d33
kevwargo <jarasz.kevwargo@gmail.com>
2014-06-02 13:21:39 +0000

    commit 2

--- PWM/pwm.s
+++ PWM/pwm.s
@@ -4,16 +4,17 @@
 .equ    PERIOD  =   10
 .equ    PWM_MAX =   1000
 
+.equ    LEFT_TURN_FLAG  =   1
+.equ    RIGHT_TURN_FLAG =   2
+
+
+.def    TEMP    =   r0
+
 .def    BUF     =   r16
 .def    ARG1    =   r17
 .def    ARG2    =   r18
-.def    TEMP    =   r0
 .def    ADC_RESULT      =   r19
-.def    ADC_RESULT_1    =   r20
-.def    ADC_RESULT_2    =   r21
-.def    ADC_RESULT_3    =   r22
-.def    ADC_RESULT_4    =   r23
-.def    ADC_RESULT_5    =   r24
+.def    TURN_FLAGS      =   r20
 
 #define     COM_RIGHT0     COM1A0
 #define     COM_RIGHT1     COM1A1
@@ -30,7 +31,7 @@
 .org 0x03
         reti                    ; unused interrupt
 .org 0x04
-        reti                    ; unused interrupt
+        rjmp t2_ovf_handle
 .org 0x05
         reti                    ; unused interrupt
 .org 0x06
@@ -85,31 +86,49 @@ init:
 main:
         
         rcall ADC_setup
+
+        ;; sbi PORTB, PORTB1
+        ;; sbi PORTB, PORTB2
+        
         rcall PWM_setup
 
-        ldi ARG1, LOW(200)
-        ldi ARG2, HIGH(200)
+        ldi ARG1, LOW(750)
+        ldi ARG2, HIGH(750)
         rcall left_PWM
 
-        ldi ARG1, LOW(800)
-        ldi ARG2, HIGH(800)
+        ldi ARG1, LOW(700)
+        ldi ARG2, HIGH(700)
         rcall right_PWM
 
         rcall left_on
         rcall right_on
 
+        clr TURN_FLAGS
+
 mainloop:
         rcall ADC_read
-        cpi ADC_RESULT, 0x01
-        brne white_1
-        rcall right_off
-        rcall left_on
-white_1:
-        cpi ADC_RESULT, 0x10
-        brne white_5
-        rcall left_off
-        rcall right_on
-white_5:        
+        cpi ADC_RESULT, 0b11111
+        breq mainloop
+
+        mov BUF, ADC_RESULT
+        andi BUF, 0b11000
+        breq white_left
+        ;; cbi PORTD, PORTD6
+        rcall turn_left
+        rjmp mainloop
+white_left:
+        mov BUF, ADC_RESULT
+        andi BUF, 0b00011
+        breq white_right
+        ;; cbi PORTD, PORTD6
+        rcall turn_right
+        rjmp mainloop
+white_right:
+        ;; cpi ADC_RESULT, 0b00100
+        ;; brne white_3
+        ;; sbi PORTD, PORTD6
+        rcall forward
+white_3:
         rjmp mainloop
 
 
@@ -142,6 +161,7 @@ PWM_setup:
         ldi BUF, (1<<COM_RIGHT1) | (1<<COM_LEFT1) | (1<<WGM11)
         ;; ldi BUF, (1<<COM_RIGHT1) | (1<<COM_LEFT0) | (1<<COM_LEFT1) | (1<<WGM11)
         ;; ldi BUF, (1<<COM_RIGHT1) | (1<<COM_RIGHT0) | (1<<COM_LEFT1) | (1<<WGM11)
+        ;; ldi BUF, (1<<COM_LEFT1) | (1<<WGM11)
         out TCCR1A, BUF
         ldi BUF, (1<<WGM12) | (1<<WGM13) | (1<<CS12)
         out TCCR1B, BUF
@@ -158,13 +178,29 @@ right_PWM:
         ret
 
 left_on:
+        sbic PORTD, PORTD0
+        rjmp _left_on
+        sbis PORTD, PORTD1
+        rjmp _left_on
+        rcall left_off
+        ldi ARG1, 5
+        rcall msleep
+_left_on:
         sbi PORTD, PORTD0
         cbi PORTD, PORTD1
         ret
 
 left_rev:
-        sbi PORTD, PORTD1
+        sbis PORTD, PORTD0
+        rjmp _left_rev
+        sbic PORTD, PORTD1
+        rjmp _left_rev
+        rcall left_off
+        ldi ARG1, 5
+        rcall msleep
+_left_rev:
         cbi PORTD, PORTD0
+        sbi PORTD, PORTD1
         ret
 
 left_off:
@@ -173,18 +209,83 @@ left_off:
         ret
 
 right_on:
+        sbic PORTD, PORTD2
+        rjmp _right_on
+        sbis PORTD, PORTD3
+        rjmp _right_on
+        rcall right_off
+        ldi ARG1, 5
+        rcall msleep
+_right_on:
         sbi PORTD, PORTD2
         cbi PORTD, PORTD3
         ret
 
 right_rev:
-        sbi PORTD, PORTD3
+        sbis PORTD, PORTD2
+        rjmp _right_rev
+        sbic PORTD, PORTD3
+        rjmp _right_rev
+        rcall right_off
+        ldi ARG1, 5
+        rcall msleep
+_right_rev:
         cbi PORTD, PORTD2
+        sbi PORTD, PORTD3
         ret
 
 right_off:
         cbi PORTD, PORTD2
         cbi PORTD, PORTD3
+        ret
+
+turn_left:
+        cbr TURN_FLAGS, RIGHT_TURN_FLAG
+        sbrc TURN_FLAGS, LEFT_TURN_FLAG
+        rjmp _turn_left_skip_sleep
+        ldi ARG1, LOW(100)
+        ldi ARG2, HIGH(100)
+        rcall left_PWM
+        rcall left_rev
+        ldi ARG1, 10
+        rcall sleep
+_turn_left_skip_sleep:
+        rcall left_off
+        ldi ARG1, LOW(1000)
+        ldi ARG2, HIGH(1000)
+        rcall right_PWM
+        rcall right_on
+        ret
+
+turn_right:
+        cbr TURN_FLAGS, LEFT_TURN_FLAG
+        sbrc TURN_FLAGS, RIGHT_TURN_FLAG
+        rjmp _turn_right_skip_sleep
+        sbr TURN_FLAGS, RIGHT_TURN_FLAG
+        ldi ARG1, LOW(100)
+        ldi ARG2, HIGH(100)
+        rcall right_PWM
+        rcall right_rev
+        ldi ARG1, 10
+        rcall sleep
+_turn_right_skip_sleep:
+        rcall right_off
+        ldi ARG1, LOW(1000)
+        ldi ARG2, HIGH(1000)
+        rcall left_PWM
+        rcall left_on
+        ret
+
+forward:
+        clr TURN_FLAGS
+        ldi ARG1, LOW(750)
+        ldi ARG2, HIGH(750)
+        rcall left_PWM
+        ldi ARG1, LOW(700)
+        ldi ARG2, HIGH(700)
+        rcall right_PWM
+        rcall left_on
+        rcall right_on
         ret
 
 
@@ -196,8 +297,8 @@ ADC_setup:
         ret
 
 ADC_read:
-        ldi BUF, (1<<SE) | (1<<SM0)
-        out MCUCR, BUF
+        ;; ldi BUF, (1<<SE) | (1<<SM0)
+        ;; out MCUCR, BUF
         sbi ADCSRA, ADIE
 
         clr ADC_RESULT
@@ -207,8 +308,8 @@ ADC_read:
         cbi ADMUX, MUX2
         sbi ADCSRA, ADSC
         sleep
-        in ADC_RESULT_1, ADCH
-        cpi ADC_RESULT_1, 220
+        in BUF, ADCH
+        cpi BUF, 220
         brlo _1_white
         ori ADC_RESULT, 0x01
 
@@ -218,8 +319,8 @@ _1_white:
         cbi ADMUX, MUX2
         sbi ADCSRA, ADSC
         sleep
-        in ADC_RESULT_2, ADCH
-        cpi ADC_RESULT_2, 220
+        in BUF, ADCH
+        cpi BUF, 220
         brlo _2_white
         ori ADC_RESULT, 0x02
 
@@ -229,8 +330,8 @@ _2_white:
         cbi ADMUX, MUX2
         sbi ADCSRA, ADSC
         sleep
-        in ADC_RESULT_3, ADCH
-        cpi ADC_RESULT_3, 220
+        in BUF, ADCH
+        cpi BUF, 220
         brlo _3_white
         ori ADC_RESULT, 0x04
 
@@ -240,8 +341,8 @@ _3_white:
         sbi ADMUX, MUX2
         sbi ADCSRA, ADSC
         sleep
-        in ADC_RESULT_4, ADCH
-        cpi ADC_RESULT_4, 220
+        in BUF, ADCH
+        cpi BUF, 220
         brlo _4_white
         ori ADC_RESULT, 0x08
 
@@ -251,16 +352,16 @@ _4_white:
         sbi ADMUX, MUX2
         sbi ADCSRA, ADSC
         sleep
-        in ADC_RESULT_5, ADCH
-        cpi ADC_RESULT_5, 220
+        in BUF, ADCH
+        cpi BUF, 220
         brlo _5_white
         ori ADC_RESULT, 0x10
 
 _5_white:       
 
         cbi ADCSRA, ADIE
-        ldi BUF, (1<<SE)
-        out MCUCR, BUF
+        ;; ldi BUF, (1<<SE)
+        ;; out MCUCR, BUF
 
         ;; clr ADC_RESULT
         
@@ -275,6 +376,10 @@ t0_ovf_handle:
 ;;         brne eoi
 ;;         ldi ARG1, PERIOD
 ;; eoi:
+        reti
+
+t2_ovf_handle:
+        
         reti
 
 adc_complete:
